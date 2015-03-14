2d888c36e484ff3522d383ad864fc4fa240c3d34
Haley Bash <halebash@Haley-Bashs-MacBook-Pro.local>
2015-03-14 17:38:17 -0400

    Refactored code

--- Easy/01_firstreverse.rb
+++ Easy/01_firstreverse.rb
@@ -1,10 +1,3 @@
-# I know there's the reverse method, but that didn't feel like  enough of
-# a cranial exercise for my first challenge, so in this solution I played
-# around with brace one-liner notation.
-
 def FirstReverse(str)
-	half_length = str.length / 2
-	letters = str.split(//)
-	half_length.times { |i| letters[i], letters[-i-1] = letters[-i-1], letters[i] }
-	letters.join
+	letters = str.split(//).reverse.join
 end
